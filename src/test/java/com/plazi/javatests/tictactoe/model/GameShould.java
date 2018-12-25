b5cd5c9e8dab3bd5aa10dd757b59e4da193e3a1c
fmaylinch <ferranmaylinch@gmail.com>
2018-12-25 21:04:27 +0100

    TicTacToe - Game should switch players (simplify)

--- src/test/java/com/plazi/javatests/tictactoe/model/GameShould.java
+++ src/test/java/com/plazi/javatests/tictactoe/model/GameShould.java
@@ -8,26 +8,15 @@ import static org.junit.Assert.*;
 public class GameShould {
 
     @Test
-    public void start_with_player_X() {
+    public void switch_player_after_each_move() {
 
         final Game game = new Game();
         assertThat(game.getNextPlayer(), is(Tile.X));
-    }
-
-    @Test
-    public void switch_to_player_O_after_first_move() {
-
-        final Game game = new Game();
         game.placeTile(0, 0);
         assertThat(game.getNextPlayer(), is(Tile.O));
-    }
-
-    @Test
-    public void switch_back_to_player_X_after_second_move() {
-
-        final Game game = new Game();
-        game.placeTile(0, 0);
         game.placeTile(1, 1);
         assertThat(game.getNextPlayer(), is(Tile.X));
+        game.placeTile(2, 2);
+        assertThat(game.getNextPlayer(), is(Tile.O));
     }
 }
