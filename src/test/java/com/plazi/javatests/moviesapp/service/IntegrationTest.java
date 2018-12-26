401bc7dcf7e6a1ee188f54e7aa08200dda047c67
fmaylinch <ferranmaylinch@gmail.com>
2018-12-26 11:34:22 +0100

    Movies app - Integration test setUp/tearDown

--- src/test/java/com/plazi/javatests/moviesapp/service/IntegrationTest.java
+++ src/test/java/com/plazi/javatests/moviesapp/service/IntegrationTest.java
@@ -4,6 +4,8 @@ import com.plazi.javatests.moviesapp.data.MovieRepository;
 import com.plazi.javatests.moviesapp.data.MovieRepositoryJdbc;
 import com.plazi.javatests.moviesapp.model.Genre;
 import com.plazi.javatests.moviesapp.model.Movie;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -19,16 +21,23 @@ import static org.junit.Assert.*;
 
 public class IntegrationTest {
 
-    @Test
-    public void retrieve_movies() throws Exception {
+    private DataSource dataSource;
+    private MovieService movieService;
 
-        final DataSource dataSource = new DriverManagerDataSource("jdbc:h2:~/test;MODE=MYSQL", "sa", "sa");
+    @Before
+    public void setUp() throws Exception {
+
+        dataSource = new DriverManagerDataSource("jdbc:h2:~/test;MODE=MYSQL", "sa", "sa");
 
         ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/init.sql"));
         ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/test-data.sql"));
 
         final MovieRepository movieRepository = new MovieRepositoryJdbc(dataSource);
-        final MovieService movieService = new MovieService(movieRepository);
+        movieService = new MovieService(movieRepository);
+    }
+
+    @Test
+    public void retrieve_movies() throws Exception {
 
         final Collection<Movie> movies = movieService.findAll();
         assertThat(movies, is(Arrays.asList(
@@ -36,6 +45,10 @@ public class IntegrationTest {
                 new Movie(2, "Memento", 113, Genre.THRILLER),
                 new Movie(3, "Matrix", 136, Genre.ACTION)
         )));
+    }
+
+    @After
+    public void tearDown() throws Exception {
 
         // Remove H2 files -- https://stackoverflow.com/a/51809831/1121497
         final Statement s = dataSource.getConnection().createStatement();
