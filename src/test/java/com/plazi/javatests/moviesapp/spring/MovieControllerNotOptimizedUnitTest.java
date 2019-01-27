958280302aa07fcad8ddc96b373fbd66f2608040
fmaylinch <ferranmaylinch@gmail.com>
2019-01-27 14:07:04 -0500

    Movies app - Add test variation that checks json instead of Movie objects

--- src/test/java/com/plazi/javatests/moviesapp/spring/MovieControllerNotOptimizedUnitTest.java
+++ src/test/java/com/plazi/javatests/moviesapp/spring/MovieControllerNotOptimizedUnitTest.java
@@ -3,10 +3,12 @@ package com.plazi.javatests.moviesapp.spring;
 import com.plazi.javatests.moviesapp.model.Genre;
 import com.plazi.javatests.moviesapp.model.Movie;
 import com.plazi.javatests.moviesapp.service.MovieService;
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,6 +19,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
@@ -56,5 +59,25 @@ public class MovieControllerNotOptimizedUnitTest {
         List<Movie> movies = response.getBody();
 
         Assert.assertThat(movies, is(sampleMovies));
+    }
+
+    @Test
+    public void getMovies_json() throws Exception {
+
+        List<Movie> sampleMovies = Arrays.asList(
+                new Movie(1, "Memento", 113, Genre.THRILLER),
+                new Movie(2, "Super 8", 112, Genre.THRILLER),
+                new Movie(3, "Matrix", 136, Genre.ACTION)
+        );
+
+        Mockito.when(movieService.findAll()).thenReturn(sampleMovies);
+
+        String responseJson = restTemplate.getForObject(
+                "http://localhost:" + port + "/api/movies", String.class);
+
+        final String expectedMoviesJson =
+                IOUtils.resourceToString("/json/expected-movies.json", StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals(expectedMoviesJson, responseJson, true);
     }
 }
